4791bf622f5106d64b9f930296959f9b6737b505
caio <caioguilhermeuna@gmail.com>
2016-06-21 15:54:41 -0300

    primeiro

---
+++ Novo1.pas
@@ -0,0 +1,11 @@
+Program Pzim ;
+	var a,b:integer;
+Begin
+	write('Digite os numeros:');
+	readln(a,b);
+	writeln('Soma: ',a+b);
+	writeln('Subtraçao: ',a-b);
+	writeln('Mutiplicaçao: ',a*b:5:2);
+	writeln('Divisao: ',a/b:5:2);
+	readln;  
+End.
