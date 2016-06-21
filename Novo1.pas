8adbd2416be160da2356ca46c9792c0c4ff638ad
caio <caioguilhermeuna@gmail.com>
2016-06-21 16:22:45 -0300

    segundo

--- Novo1.pas
+++ Novo1.pas
@@ -1,11 +1,15 @@
 Program Pzim ;
-	var a,b:integer;
+	var a,b,c:integer;
 Begin
 	write('Digite os numeros:');
 	readln(a,b);
-	writeln('Soma: ',a+b);
-	writeln('Subtraçao: ',a-b);
-	writeln('Mutiplicaçao: ',a*b:5:2);
-	writeln('Divisao: ',a/b:5:2);
+	write('1:Soma ,2:Subtraçao ,3:Mutiplicaçao ,4:Divisao: ');
+	readln(c);
+	case c Of
+		1: writeln('Soma: ',a+b);
+		2: writeln('Subtraçao: ',a-b);
+		3: writeln('Mutiplicaçao: ',a*b:5:2);
+		4: writeln('Divisao: ',a/b:5:2);		
+	end;
 	readln;  
 End.
